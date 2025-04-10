dea7e71e14bebb638fe4b1f9bbb2712586ba4794
Bruno Ferreira <brunofss1985@gmail.com>
2025-04-10 10:59:28 -0300

    tornando o componente de tabela dinamica e reutilizavel

--- src/app/admin/pages/usuarios/usuarios.component.ts
+++ src/app/admin/pages/usuarios/usuarios.component.ts
@@ -9,6 +9,14 @@ export class UsuariosComponent implements OnInit {
 
   constructor() { }
 
+  headers = ['ID', 'Nome', 'Email', 'Tipo de Usuário'];
+dados = [
+  { ID: 1, Nome: 'Mark', Email: 'Otto' },
+  { ID: 2, Nome: 'Jacob', Email: 'Thornton', endereço: 'rua iperó' },
+  { ID: 2, Nome: 'Jacob', Email: 'Thornton' },
+  { ID: 2, Nome: 'Jacob', Último: 'Thornton' },
+]
+
   ngOnInit(): void {
   }
 
--- src/app/shared/tables/tables.component.ts
+++ src/app/shared/tables/tables.component.ts
@@ -7,6 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TablesComponent implements OnInit {
 
+  @Input() headers: string[] = [];
+  @Input() data: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {

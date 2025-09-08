cbcb934f67a19da03b52f0ff898f2b53e7eab321
Bruno Ferreira <brunofss1985@gmail.com>
2025-09-08 11:23:02 -0300

    atualizacao no angularjson

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://192.168.1.4:8080',
   mercadoPagoPublicKey: 'TEST-ca476526-1d41-4d7e-a55c-a43c21fbe404',
 };

629bceabb46d0c7ad878db19a8e27fb1febba2b0
Bruno Ferreira <brunofss1985@gmail.com>
2025-09-08 18:54:12 -0300

    atualizando deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://192.168.1.4:8080',
+  production: true,
+  apiUrl: 'https://loja-api-558ec62c308a.herokuapp.com',
   mercadoPagoPublicKey: 'TEST-ca476526-1d41-4d7e-a55c-a43c21fbe404',
 };

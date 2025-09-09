970762291ab2cd56bbc4f7265e3fbde4fb3d1e2a
Bruno Ferreira <brunofss1985@gmail.com>
2025-09-08 22:18:34 -0300

    ajustes no authservice e cartservice

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://loja-api-558ec62c308a.herokuapp.com',
+ apiUrl: 'https://loja-api-558ec62c308a.herokuapp.com/',
   mercadoPagoPublicKey: 'TEST-ca476526-1d41-4d7e-a55c-a43c21fbe404',
 };

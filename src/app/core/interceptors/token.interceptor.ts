74ce0299b8eae9a13d45a2bca353ecfbb83eb9a6
Bruno Ferreira <brunofss1985@gmail.com>
2025-09-11 15:55:10 -0300

    mensagem de erro apos token expirar

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -3,16 +3,22 @@ import {
   HttpInterceptor,
   HttpRequest,
   HttpHandler,
-  HttpEvent
+  HttpEvent,
+  HttpErrorResponse,
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from '../services/authService/auth.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.authService.getToken();
@@ -22,10 +28,16 @@ export class TokenInterceptor implements HttpInterceptor {
       : req;
 
     return next.handle(authReq).pipe(
-      catchError(err => {
+      catchError((err: HttpErrorResponse) => {
         if (err.status === 401) {
+          this.toastr.warning(
+            'Sua sessão expirou. Faça login novamente.',
+            'Sessão Expirada',
+            { timeOut: 4000, positionClass: 'toast-top-right' }
+          );
+
           this.authService.clearSession();
-          this.router.navigate(['/public/default-login/login']);
+          this.router.navigate(['/default-login/login']);
         }
         return throwError(() => err);
       })

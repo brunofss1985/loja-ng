43a6424702e757b7caa2d4c279d06f74633d6795
Bruno Ferreira <brunofss1985@gmail.com>
2025-09-12 18:34:45 -0300

    teste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
-  production: false,
+  production: true,
   apiUrl: 'https://loja-api-558ec62c308a.herokuapp.com',
   mercadoPagoPublicKey: 'TEST-ca476526-1d41-4d7e-a55c-a43c21fbe404',
 };
